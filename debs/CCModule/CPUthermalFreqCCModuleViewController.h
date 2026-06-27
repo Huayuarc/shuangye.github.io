81d1ada33f0389085946bcdfe9fb0be2941ff95c
Huayuarc <738985817@qq.com>
2026-06-28 03:53:51 +0800

    chore: 推送 Systempro 源码（CI 编译）

--- debs/CCModule/CPUthermalFreqCCModuleViewController.h
+++
@@ -1,8 +0,0 @@
-#import "CCUIHeaders.h"
-
-NS_ASSUME_NONNULL_BEGIN
-
-@interface CPUthermalFreqCCModuleViewController : UIViewController <CCUIContentModuleContentViewController>
-@end
-
-NS_ASSUME_NONNULL_END
