18e372a911064af08762aaf111dcfc9db82ca4e6
Huayuarc <108604483+Huayuarc@users.noreply.github.com>
2026-07-24 00:31:27 +0800

    Delete debs/insulation-l directory

--- debs/insulation-l/InsulationPrefs/Sources/InsulationPrefsC/include/PSSpecifier/PSSpecifier.h
+++
@@ -1,6 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <Preferences/PSSpecifier.h>
-
-@interface PSSpecifier (beHugeInsulation)
-- (void)setValues:(id)values titles:(id)titles;
-@end
--- debs/insulation-l/Sources/insulationC/include/Power_header/HidSensors.h
+++
@@ -1,7 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <UIKit/UIKit.h>
-
-@interface HidSensors
-+ (id)sharedInstance;
-- (void)handleTemperatureEvent:(int)a3 service:(id)a4;
-@end
