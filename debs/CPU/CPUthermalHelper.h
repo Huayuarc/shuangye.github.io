2f265b7570eb2765be891933fa8d96e4d5ba047d
shuangye <shuangye@github.com>
2026-07-27 00:44:05 +0800

    chore: 推送源码 - CPUthermal (CI 编译)

---
+++ debs/CPU/CPUthermalHelper.h
@@ -0,0 +1,20 @@
+#import <Foundation/Foundation.h>
+#import "Headers/Tweak.h"
+
+@interface CPUthermalHelper : NSObject
+
+@property (nonatomic, strong) NSDictionary *plistObj;
+@property (nonatomic, strong) CommonProduct *commonProductObject;
+@property (nonatomic, assign) BOOL thermalPreventDimmingEnabled;
++ (instancetype)shared;
+
+- (void)getLocalPrefValue;
+- (void)reloadPrefs;
+- (int)getCPUMaxPower;
+
+- (CFDictionaryRef)patchThermalPlist:(CFDictionaryRef)cfDict;
+
+// 工具方法
++ (BOOL)userspaceReboot;
+
+@end
