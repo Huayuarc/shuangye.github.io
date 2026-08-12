b8e5c9959fe73a28d02e985f3ebba2184e2c2f86
Huayuarc <huayuarc@users.noreply.github.com>
2026-08-12 12:14:48 +0800

    chore: 更新图片视频模块名称和署名

---
+++ debs/PictureModule16/PictureModule2.m
@@ -0,0 +1,9 @@
+#import "PMOriginal.h"
+@interface PictureModule2:PMOriginalModule @end
+@implementation PictureModule2
+- (instancetype)init { return [super initWithIdentifier:@"com.huayuarc.picturemodule16.2"]; }
+@end
+@interface PictureModule2ListController:PMOriginalListController @end
+@implementation PictureModule2ListController
+- (instancetype)init { return [super initWithIdentifier:@"com.huayuarc.picturemodule16.2"]; }
+@end
---
+++ debs/PictureModule16/PictureModule4.m
@@ -0,0 +1,9 @@
+#import "PMOriginal.h"
+@interface PictureModule4:PMOriginalModule @end
+@implementation PictureModule4
+- (instancetype)init { return [super initWithIdentifier:@"com.huayuarc.picturemodule16.4"]; }
+@end
+@interface PictureModule4ListController:PMOriginalListController @end
+@implementation PictureModule4ListController
+- (instancetype)init { return [super initWithIdentifier:@"com.huayuarc.picturemodule16.4"]; }
+@end
