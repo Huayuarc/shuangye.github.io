b4ce71d69d3623ac96376d8eafb9ba012f8760fe
Huayuarc <huayuarc@users.noreply.github.com>
2026-08-13 09:46:52 +0800

    fix: 媒体与偏好适配 RootHide 动态隐根

---
+++ debs/PictureModule16/PictureModule3.m
@@ -0,0 +1,9 @@
+#import "PMOriginal.h"
+@interface PictureModule3:PMOriginalModule @end
+@implementation PictureModule3
+- (instancetype)init { return [super initWithIdentifier:@"com.huayuarc.picturemodule16.3"]; }
+@end
+@interface PictureModule3ListController:PMOriginalListController @end
+@implementation PictureModule3ListController
+- (instancetype)init { return [super initWithIdentifier:@"com.huayuarc.picturemodule16.3"]; }
+@end
