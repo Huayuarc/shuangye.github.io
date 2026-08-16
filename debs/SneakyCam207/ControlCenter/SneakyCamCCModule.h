b8b556ef66bd62b4d3e908070b7900dc6a3dc0df
Huayuarc <huayuarc@users.noreply.github.com>
2026-08-16 16:19:02 +0800

    fix: support iOS 15-17 deployment; audit APIs and minimum OS

---
+++ debs/SneakyCam207/ControlCenter/SneakyCamCCModule.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+#import <ControlCenterUIKit/ControlCenterUIKit.h>
+
+@interface SneakyCamCCPhoto : CCUIToggleModule
+@end
+@interface SneakyCamCCVideo : CCUIToggleModule
+@end
