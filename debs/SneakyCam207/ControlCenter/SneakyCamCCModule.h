0bdfcf981b53dd9da73b76d5decc08f207606d48
Huayuarc <huayuarc@users.noreply.github.com>
2026-08-17 13:11:08 +0800

    fix: async session readiness and SpringBoard camera access for rootless

---
+++ debs/SneakyCam207/ControlCenter/SneakyCamCCModule.h
@@ -0,0 +1,4 @@
+#import "SneakyCamCCCommon.h"
+@interface SneakyCamCCToggle : NSObject <CCUIContentModule> @end
+@interface SneakyCamCCPhoto : NSObject <CCUIContentModule> @end
+@interface SneakyCamCCVideo : NSObject <CCUIContentModule> @end
