d0b57f87e64387a8716fe7e64579169695c42ff6
Huayuarc <huayuarc@users.noreply.github.com>
2026-08-22 11:11:41 +0800

    fix: trigger haptic immediately on volume double-press actions

---
+++ debs/SneakyCam207/ControlCenter/SneakyCamCCVideo.m
@@ -0,0 +1,5 @@
+#import "SneakyCamCCModule.h"
+@implementation SneakyCamCCVideo { UIViewController<CCUIContentModuleContentViewController> *_vc; }
+- (UIViewController<CCUIContentModuleContentViewController> *)contentViewController { if(!_vc)_vc=(id)SneakyCamCCCreateViewController(SneakyCamCCModeVideo);return _vc; }
+- (UIViewController *)backgroundViewController{return nil;}
+@end
