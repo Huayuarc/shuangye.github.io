d4867c8e3b50e0d7a5fb0170774909c720cb1a67
Huayuarc <huayuarc@users.noreply.github.com>
2026-08-24 22:13:51 +0800

    fix: PictureModule 1.2.2 避免 PreferenceLoader 图标冲突

---
+++ debs/PictureModule16/PictureModule4.m
@@ -0,0 +1,9 @@
+#import "PMOriginal.h"
+@interface PictureModule4:PMOriginalModule @end
+@implementation PictureModule4
+- (instancetype)init { return [super initWithIdentifier:@"com.4nni3.picturemodule_4"]; }
+@end
+@interface PictureModule4ListController:PMOriginalListController @end
+@implementation PictureModule4ListController
+- (instancetype)init { return [super initWithIdentifier:@"com.4nni3.picturemodule_4"]; }
+@end
