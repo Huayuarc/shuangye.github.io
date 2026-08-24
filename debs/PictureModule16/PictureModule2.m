afbc5a8a5e7798b907fe83f69360b982fefabc5c
Huayuarc <huayuarc@users.noreply.github.com>
2026-08-24 16:57:25 +0800

    fix: PictureModule 1.1.8 恢复控制中心设置入口

---
+++ debs/PictureModule16/PictureModule2.m
@@ -0,0 +1,9 @@
+#import "PMOriginal.h"
+@interface PictureModule2:PMOriginalModule @end
+@implementation PictureModule2
+- (instancetype)init { return [super initWithIdentifier:@"com.4nni3.picturemodule_2"]; }
+@end
+@interface PictureModule2ListController:PMOriginalListController @end
+@implementation PictureModule2ListController
+- (instancetype)init { return [super initWithIdentifier:@"com.4nni3.picturemodule_2"]; }
+@end
